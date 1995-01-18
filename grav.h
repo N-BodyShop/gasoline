9e64d485b02e587498c062fa7eebe343d5005a1f
stadel <stadel>
1995-01-18 23:07:12 +0000

    Changed local variable names in macro SPLINE to avoid conflicts

--- grav.h
+++ grav.h
@@ -9,36 +9,39 @@
  ** 
  ** Higher derivative terms c and d for use with quadrupole spline
  ** softening (Joachim Stadel, Dec. 94).
+ ** Had to append SPLINE_ to the local variables to avoid the case that one
+ ** of the passed variables to the macro has the same name. It looks pretty
+ ** unreadable now, oh well. (Joachim Stadel, Jan. 95)
  */
 #define SPLINE(r2,twoh,a,b,c,d)\
 {\
-	double r,u,dih,dir;\
-	r = sqrt(r2);\
-	if (r < twoh) {\
-		dih = 2.0/twoh;\
-		u = r*dih;\
-		if (u < 1.0) {\
-			a = dih*(7.0/5.0 - 2.0/3.0*u*u + 3.0/10.0*u*u*u*u\
-					 - 1.0/10.0*u*u*u*u*u);\
-			b = dih*dih*dih*(4.0/3.0 - 6.0/5.0*u*u + 1.0/2.0*u*u*u);\
-		    c = dih*dih*dih*dih*dih*(12.0/5.0 - 3.0/2.0*u);\
-			if (r > 0.0) d = 3.0/2.0*dih*dih*dih*dih*dih*dih/r;\
+	double SPLINE_r,SPLINE_u,SPLINE_dih,SPLINE_dir;\
+	SPLINE_r = sqrt(r2);\
+	if (SPLINE_r < twoh) {\
+		SPLINE_dih = 2.0/twoh;\
+		SPLINE_u = SPLINE_r*SPLINE_dih;\
+		if (SPLINE_u < 1.0) {\
+			a = SPLINE_dih*(7.0/5.0 - 2.0/3.0*SPLINE_u*SPLINE_u + 3.0/10.0*SPLINE_u*SPLINE_u*SPLINE_u*SPLINE_u\
+					 - 1.0/10.0*SPLINE_u*SPLINE_u*SPLINE_u*SPLINE_u*SPLINE_u);\
+			b = SPLINE_dih*SPLINE_dih*SPLINE_dih*(4.0/3.0 - 6.0/5.0*SPLINE_u*SPLINE_u + 1.0/2.0*SPLINE_u*SPLINE_u*SPLINE_u);\
+		    c = SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*(12.0/5.0 - 3.0/2.0*SPLINE_u);\
+			if (r > 0.0) d = 3.0/2.0*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih/r;\
 			else d = 0.0;\
 			}\
 		else {\
-			dir = 1.0/r;\
-			a = -1.0/15.0*dir + dih*(8.0/5.0 - 4.0/3.0*u*u + u*u*u\
-			              - 3.0/10.0*u*u*u*u + 1.0/30.0*u*u*u*u*u);\
-			b = -1.0/15.0*dir*dir*dir + dih*dih*dih*(8.0/3.0 - 3.0*u + 6.0/5.0*u*u - 1.0/6.0*u*u*u);\
-			c = -1.0/5.0*dir*dir*dir*dir*dir + 3.0*dih*dih*dih*dih*dir\
-				+ dih*dih*dih*dih*dih*(-12.0/5.0 + 1.0/2.0*u);\
-			d = -dir*dir*dir*dir*dir*dir*dir\
-				+ 3.0*dih*dih*dih*dih*dir*dir*dir\
-					- 1.0/2.0*dih*dih*dih*dih*dih*dih*dir;\
+			SPLINE_dir = 1.0/SPLINE_r;\
+			a = -1.0/15.0*SPLINE_dir + SPLINE_dih*(8.0/5.0 - 4.0/3.0*SPLINE_u*SPLINE_u + SPLINE_u*SPLINE_u*SPLINE_u\
+			              - 3.0/10.0*SPLINE_u*SPLINE_u*SPLINE_u*SPLINE_u + 1.0/30.0*SPLINE_u*SPLINE_u*SPLINE_u*SPLINE_u*SPLINE_u);\
+			b = -1.0/15.0*SPLINE_dir*SPLINE_dir*SPLINE_dir + SPLINE_dih*SPLINE_dih*SPLINE_dih*(8.0/3.0 - 3.0*SPLINE_u + 6.0/5.0*SPLINE_u*SPLINE_u - 1.0/6.0*SPLINE_u*SPLINE_u*SPLINE_u);\
+			c = -1.0/5.0*SPLINE_dir*SPLINE_dir*SPLINE_dir*SPLINE_dir*SPLINE_dir + 3.0*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dir\
+				+ SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*(-12.0/5.0 + 1.0/2.0*SPLINE_u);\
+			d = -SPLINE_dir*SPLINE_dir*SPLINE_dir*SPLINE_dir*SPLINE_dir*SPLINE_dir*SPLINE_dir\
+				+ 3.0*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dir*SPLINE_dir*SPLINE_dir\
+					- 1.0/2.0*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dih*SPLINE_dir;\
 			}\
 		}\
 	else {\
-		a = 1.0/r;\
+		a = 1.0/SPLINE_r;\
 		b = a*a*a;\
 		c = 3.0*b*a*a;\
 		d = 5.0*c*a*a;\
