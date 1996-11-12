011ece2815f118a27e5f4de0038c3846d8540787
trq <trq>
1996-11-12 00:26:19 +0000

    Use NATIVE_SQRT on alpha and pentium.

--- grav.c
+++ grav.c
@@ -7,7 +7,8 @@
 #include "meval.h"
 #include "qeval.h"
 
-#define NATIVE_SQRT (defined(_MIPS_ISA) && (_MIPS_ISA == _MIPS_ISA_MIPS4)) 
+#define NATIVE_SQRT (defined(_MIPS_ISA) && (_MIPS_ISA == _MIPS_ISA_MIPS4) \
+		     || defined(__alpha__) || defined(__i486__)) 
 
 #if !(NATIVE_SQRT)
 void v_sqrt1(int,double *,double *);
