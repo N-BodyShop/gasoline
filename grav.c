8ee12b1bce38a4199fca6bf47938a1f3383a0f57
trq <trq>
2004-03-13 01:21:21 +0000

    Use NATIVE_SQRT on IA64 architectures.

--- grav.c
+++ grav.c
@@ -8,7 +8,7 @@
 #include "qeval.h"
 
 #define NATIVE_SQRT (defined(_MIPS_ISA) && (_MIPS_ISA == _MIPS_ISA_MIPS4) \
-	     || defined(__i486__) || defined(CCC) ) 
+	     || defined(__i486__) || defined(CCC) || defined(__ia64__)) 
 
 #if !(NATIVE_SQRT)
 void v_sqrt1(int,double *,double *);
