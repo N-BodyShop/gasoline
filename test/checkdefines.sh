2b910546b1bd5d521ec6c04c967f3479ae161bc5
Ben Keller <malzraa@gmail.com>
2013-07-01 17:24:17 -0400

    Checkpoints should work now

--- test/checkdefines.sh
+++ test/checkdefines.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MAKEDEFS=`grep "CODE_DEF +=" ../Makefile | awk '{print $3;}' | sed -e 's/-D//'`
-DEFSFOUND=`grep -h 'ifdef\|ifndef' ../*[ch] | awk '{print $2;}' |  sort | uniq | grep -v '^__i386__$' | grep -v '^__AGGS_H$' | grep -v 'HINCLUDED$' | grep -v '^COOLING_COSMO$' | grep -v '^COOLING_BATE$' | grep -v '^COOLING_METAL$'`
+MAKEDEFS=`grep "CODE_DEF +=" ../Makefile | sed -e 's/=/ /g' | awk '{print $3;}' | sed -e 's/-D//'`
+DEFSFOUND=`grep 'ifdef\|ifndef' ../*.[ch] | awk '{print $2;}' |  sort | uniq | grep -v '^__i386__$' | grep -v '^__AGGS_H$' | grep -v 'HINCLUDED$' | grep -v '^COOLING_COSMO$' | grep -v '^COOLING_BATE$' | grep -v '^COOLING_METAL$'  | grep -v "^[/][*]" | grep -v "^TRUE$" | grep -v "^FALSE$" | grep -v "^\#ifdef$" | grep -v "^Y_EMIN$"`
 PASS=true
 for DEF in $DEFSFOUND
 do
@@ -10,6 +10,10 @@ do
 	then
 		PASS=false
 		echo $DEF "Missing in Makefile!"
+		for FNAME in `grep -l $DEF ../*.[ch]`
+		do
+			echo $DEF "is in" $FNAME
+		done
 	fi
 done
 for DEF in $MAKEDEFS
