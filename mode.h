49ac4ebe5e4c459f70ce5d92d4bca43592e47a9d
Ben Keller <malzraa@gmail.com>
2017-05-08 14:17:06 -0400

    Added the basic modes for building gasoline

--- mode.h
+++ mode.h
@@ -2,18 +2,32 @@
 #define MODE_HINCLUDED
 
 #ifdef GALAXY
-GASOLINE              //Do hydro 
-JEANSSOFT             //Calculate presure floor to keep jeans length larger than softening or sph h (default is just sph h)
-PARTICLESPLIT         //Particles will now divide if they get too heavy
-SUPERBUBBLE	          //Use the Keller+ 2014 Superbubble feedback 
-STARFORM              //Make new stars according to the starformation recipe
-CHABRIER              //Use the Chabrier 2003 IMF for starformation (See DOI:10.1086/376392)  (SHOULD BE DEFAULT)
+#define GASOLINE              //Do hydro 
+#define JEANSSOFT             //Calculate presure floor to keep jeans length larger than softening or sph h (default is just sph h)
+#define PARTICLESPLIT         //Particles will now divide if they get too heavy
+#define SUPERBUBBLE	          //Use the Keller+ 2014 Superbubble feedback 
+#define STARFORM              //Make new stars according to the starformation recipe
+#define CHABRIER              //Use the Chabrier 2003 IMF for starformation (See DOI:10.1086/376392)  (SHOULD BE DEFAULT)
 #endif 
 
 #ifdef COLLISIONS
-#endif
+#define AGGS                  //Include support for aggregates (like asteroids and rubble piles) See aggs.c
+#define COLLISIONS            //Use solid-body collisions (not compatible with -DGASOLINE)
+#endif 
+
+#ifdef GLASS_IC
+#define GASOLINE              //Do hydro 
+#define GLASS                 //Use this to damp for glass initial conditions
+#endif 
+
+#ifdef TURBULENT
+#define GASOLINE              //Do hydro 
+#define OUTURBDRIVER          //Drive turbulence as in Price & Federrath 2010 with OU variables
+#endif 
 
 #ifdef NBODY
+#undef GASOLINE               //Disable SPH
 #endif
+
 
 #endif
